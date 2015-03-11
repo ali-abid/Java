b6b9befac7847d76f39b8344e7764976080f57a1
ali-abid <worktimee@gmail.com>
2015-03-11 12:17:41 +0000

    Commint on GnomeEngineerigManager class

--- working-directory/Practice/src/com/java/ali/GnomeEngineeringManager.java
+++ working-directory/Practice/src/com/java/ali/GnomeEngineeringManager.java
@@ -10,11 +10,13 @@ public class GnomeEngineeringManager implements Engineer {
 	private Engineer engineer;
 
 	public GnomeEngineeringManager() {
+		// Creating object of GnoneEngineer from Engineer Interface
 		engineer = new GnomeEngineer();
 	}
 
 	@Override
 	public void operateDevice() {
+		//Calling GnomeEngineer method
 		engineer.operateDevice();
 	}
 }
