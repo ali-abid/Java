b27ab828b4aeb4a1aca971a7a2bcfbd3c9f236a9
ali-abid <worktimee@gmail.com>
2015-03-25 10:02:47 +0000

    Adaptee class newMachine method

---
+++ working-directory/DesignPattern/src/com/adapter/NewMachine.java
@@ -0,0 +1,10 @@
+package com.adapter;
+
+//This class represent as a adaptee, 
+//Practically we assume that this newMachine method is incompatible for Manager operateDevice method.
+public class NewMachine {
+	
+	public void newMachine(){
+	System.out.println("New Machine 2015");
+	}
+}
