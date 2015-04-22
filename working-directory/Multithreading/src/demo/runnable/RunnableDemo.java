a9138756df5d4be4cc79de2f2ccd691a3ca07e21
ali-abid <worktimee@gmail.com>
2015-04-22 14:37:48 +0100

    implements Runnable Interface

--- working-directory/Multithreading/src/demo/runnable/RunnableDemo.java
+++ working-directory/Multithreading/src/demo/runnable/RunnableDemo.java
@@ -1,11 +1,20 @@
 package demo.runnable;
 
-public class RunnableDemo {
+public class RunnableDemo implements Runnable {
 
 	private Thread t;
+	private String threadName = "A";
+	
+	RunnableDemo(){
+		System.out.println("Calling Constructor...");
+	}
+	
+	public void run(){
+		System.out.println("Calling run function..");
+	}
 	
 	public void initialize(){
-		t = new Thread();
+		t = new Thread(this, threadName);
 		t.start();
 	}
 }
