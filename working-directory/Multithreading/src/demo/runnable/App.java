dbc8ee588fe0113993c90497d71e2753e6de70c5
ali-abid <worktimee@gmail.com>
2015-04-22 14:45:49 +0100

    run thread every 50milli seccond

--- working-directory/Multithreading/src/demo/runnable/App.java
+++ working-directory/Multithreading/src/demo/runnable/App.java
@@ -3,7 +3,7 @@ package demo.runnable;
 public class App {
 
 	public static void main(String[] args){
-		RunnableDemo r = new RunnableDemo();
+		RunnableDemo r = new RunnableDemo("A");
 		r.initialize();
 	}
 }
--- working-directory/Multithreading/src/demo/runnable/RunnableDemo.java
+++ working-directory/Multithreading/src/demo/runnable/RunnableDemo.java
@@ -3,15 +3,26 @@ package demo.runnable;
 public class RunnableDemo implements Runnable {
 
 	private Thread t;
-	private String threadName = "A";
+	private String threadName;;
 	
-	RunnableDemo(){
+	RunnableDemo(String name){
+		threadName = name;
 		System.out.println("Calling Constructor...");
 	}
 	
 	public void run(){
 		System.out.println("Calling run function..");
-	}
+			try {
+				for(int i = 4; i > 0; i--){
+					System.out.println("Thread Name: " + threadName + " , "+  i);
+					Thread.sleep(50);
+			}
+			}catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	
 	
 	public void initialize(){
 		t = new Thread(this, threadName);
