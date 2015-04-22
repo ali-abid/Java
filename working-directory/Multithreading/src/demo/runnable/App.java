c6427686ec5a256731c9996e4d7f07fb047e02b8
ali-abid <worktimee@gmail.com>
2015-04-22 14:47:41 +0100

    Thread A and B run one by one

--- working-directory/Multithreading/src/demo/runnable/App.java
+++ working-directory/Multithreading/src/demo/runnable/App.java
@@ -4,6 +4,8 @@ public class App {
 
 	public static void main(String[] args){
 		RunnableDemo r = new RunnableDemo("A");
+		RunnableDemo r2 = new RunnableDemo("B");
 		r.initialize();
+		r2.initialize();
 	}
 }
